d636091dcf9be073c46a3dad58a5b83306b1ab13
kenneth <kennethwarui35@gmail.com>
2026-05-04 21:26:27 +0300

    seeded and appilied fastapi

---
+++ seed.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.functions import random
+from app import session
+from app.session import engine
+from app.models import User
+
+
+Session = sessionmaker(bind=engine)
+session = Session
+
+usernames = ['Ken', 'Josh', 'Mike']
+
+if __name__ == '__main__':
+    session.query(User).delete()
+
+    print('SEEDING USERS')
+    users = []
+    for i in range(3):
+        new_username = User(user_name=random.choice(usernames))
+    
+    session.add(new_username)
+    session.commit()
+
+    print('**done**')
+
